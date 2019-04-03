465c487b111559de2b348419fe5b377765d19998
macJL <macjeanlouis08@gmail.com>
2019-04-03 09:56:29 +0200

    [US#17]Mode_de_paiement

---
+++ src/views/Mode_de_Paiement.vue
@@ -0,0 +1,20 @@
+<template>
+  <Menu ></Menu>
+  <h1>Mon Panier</h1>
+</template>
+
+
+<script>
+  import Menu from '../components/Navbar.vue';
+  export default {
+    name: "ViewMyAccount",
+    components: {
+      Menu,
+    },
+
+  }
+</script>
+
+<style scoped>
+
+</style>
