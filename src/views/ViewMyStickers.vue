c7d411c831cba88504dec7d643675b44a351af81
Matthieu LÉVÊQUE <matthieu.lev@gmail.com>
2019-04-11 11:07:14 +0200

    [US#13_T2] Améliorer l'affichage des autocollants d'un utilisateur

--- src/views/ViewMyStickers.vue
+++ src/views/ViewMyStickers.vue
@@ -4,7 +4,23 @@
     <b-container>
       <h1>Mes autocollants</h1>
       <div class="custom-card">
-        <b-table striped :items="stickers"></b-table>
+
+        <b-table striped :fields="fields" :items="stickers">
+          <template slot="index" slot-scope="data">
+            {{ data.index + 1 }}
+          </template>
+
+          <template slot="preview" slot-scope="data">
+            <b-img class="stickers" :src="require(`../img/stickers/${data.item.theme}/${data.item.color}.png`)" fluid
+                   alt="Stickers picture"></b-img>
+          </template>
+
+          <template slot="addtoCart" slot-scope="data">
+            <b-form-checkbox :id="'checkbox'+data.index+1" name="checkbox" v-model="data.item.in_order">
+            </b-form-checkbox>
+          </template>
+        </b-table>
+
       </div>
       <router-link class="button-validate" to="StickersCreation" replace>Créer un nouvel autocollant</router-link>
     </b-container>
@@ -24,6 +40,15 @@
     data() {
       return {
         stickers: [],
+        fields: [
+          'index',
+          {key: 'theme', label: 'Theme'},
+          {key: 'color', label: 'Couleur'},
+          {key: 'size', label: 'Taille'},
+          {key: 'shape', label: 'Forme'},
+          {key: 'preview', label: 'Aperçu'},
+          {key: 'addtoCart', label: 'Ajouter au panier'},
+        ],
       };
     },
     created() {
@@ -32,13 +57,13 @@
         .doc(firebase.auth().currentUser.uid)
         .collection('userStickers')
         .get()
-        .then(function(querySnapshot) {
+        .then(function (querySnapshot) {
           if (querySnapshot.empty) {
             console.log('no stickers documents found from userStickers collection');
           } else {
             // go through all the results
             querySnapshot.forEach(function (documentSnapshot) {
-              let data =documentSnapshot.data();
+              let data = documentSnapshot.data();
               console.log(data);
               self.stickers.push(data);
             });
@@ -88,15 +113,12 @@
     color: white;
   }
 
-  @media (max-width: 991px) {
-    .table {
-      display: block;
-      overflow-x: scroll;
-      overflow-y: hidden;
-    }
-  }
-
   a:hover {
     text-decoration: none;
+  }
+
+  .stickers {
+    width: auto;
+    height: 30px;
   }
 </style>
