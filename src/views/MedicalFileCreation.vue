788af9afac800e364a6055549aecaa508442e8a5
jfaucheux <julien.faucheux1@gmail.com>
2019-04-12 11:03:59 +0200

    corrections formulaire

--- src/views/MedicalFileCreation.vue
+++ src/views/MedicalFileCreation.vue
@@ -76,7 +76,7 @@
           <h2>Adresse</h2>
 
           <b-row>
-            <div class="col-md-2 col-xs-12">
+            <div class="col-md-3 col-xs-12">
               <b-form-group label="Numéro : ">
                 <b-form-input v-model="form.adress.number" type="number" name="adress.number"
                               placeholder="Numéro de voie" class="form-control"
@@ -86,7 +86,7 @@
                 </div>
               </b-form-group>
             </div>
-            <div class="col-md-7 col-xs-12">
+            <div class="col-md-6 col-xs-12">
               <b-form-group label="Nom de voie : ">
                 <b-form-input v-model="form.adress.street" name="adress.street" placeholder="Nom de voie"
                               class="form-control"
@@ -103,27 +103,6 @@
               </b-form-group>
             </div>
           </b-row>
-
-          <b-row>
-            <div class="col-md-6 col-xs-12">
-              <b-form-group label="Ville : ">
-                <b-form-input v-model="form.adress.city" name="adress.city" placeholder="Ville" class="form-control"
-                              :class="{ 'is-invalid': submitted && $v.form.adress.city.$error }"/>
-                <div v-if="submitted && !$v.form.adress.city.required" class="invalid-feedback">Ville obligatoire</div>
-              </b-form-group>
-            </div>
-            <div class="col-md-6 col-xs-12">
-              <b-form-group label="Etat/Province : ">
-                <b-form-input v-model="form.adress.state" name="adress.state" placeholder="Région / Etat"
-                              class="form-control"
-                              :class="{ 'is-invalid': submitted && $v.form.adress.state.$error }"/>
-                <div v-if="submitted && !$v.form.adress.state.required" class="invalid-feedback">Région / Etat
-                  obligatoire
-                </div>
-              </b-form-group>
-            </div>
-          </b-row>
-
           <b-row>
             <div class="col-md-6 col-xs-12">
               <b-form-group label="Code Postal : ">
@@ -133,6 +112,21 @@
                 <div v-if="submitted && !$v.form.adress.postal_code.required" class="invalid-feedback">Code Postal
                   obligatoire
                 </div>
+              </b-form-group>
+            </div>
+            <div class="col-md-6 col-xs-12">
+              <b-form-group label="Ville : ">
+                <b-form-input v-model="form.adress.city" name="adress.city" placeholder="Ville" class="form-control"
+                              :class="{ 'is-invalid': submitted && $v.form.adress.city.$error }"/>
+                <div v-if="submitted && !$v.form.adress.city.required" class="invalid-feedback">Ville obligatoire</div>
+              </b-form-group>
+            </div>
+          </b-row>
+          <b-row>
+            <div class="col-md-6 col-xs-12">
+              <b-form-group label="Région/Etat : ">
+                <b-form-input v-model="form.adress.state" name="adress.state" placeholder="Région / Etat"
+                              class="form-control"/>
               </b-form-group>
             </div>
             <div class="col-md-6 col-xs-12">
@@ -377,10 +371,8 @@
             numeric,
           },
           street: {required},
-          complement: {required},
           postal_code: {required},
           city: {required},
-          state: {required},
           country: {required},
         },
       },
