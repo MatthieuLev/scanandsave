c7dd5b173ab5bb7784d0ce43fd151b1458ef8dd9
macJL <macjeanlouis08@gmail.com>
2019-04-12 16:10:32 +0200

    [US#8]visualaisation des informations personnelles contact

--- src/views/ViewIContact.vue
+++ src/views/ViewIContact.vue
@@ -2,12 +2,23 @@
   <b-container>
     <div>
       <b-row>
-        <b-col>Maladie</b-col>
+        <b-col>
+          <h2>Proche à contacter</h2>
+        </b-col>
       </b-row>
     </div>
     <b-row>
-
+      <b-col>
+        <h2>Mèdecin traitant</h2>
+      </b-col>
     </b-row>
+    <b-col>
+      <b-row>
+        <p>{{this.form.last_name}}</p>
+        <p>{{this.form.first_name}}</p>
+
+      </b-row>
+    </b-col>
   </b-container>
 
 
