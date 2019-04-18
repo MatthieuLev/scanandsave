e630779710843f6cf8b679b80f07796bdb91f8da
Matthieu LÉVÊQUE <matthieu.lev@gmail.com>
2019-04-18 10:24:47 +0200

    [TECH] Corrige une erreur de bouton submit hors du formulaire

--- src/views/StickersCreation.vue
+++ src/views/StickersCreation.vue
@@ -4,94 +4,72 @@
     <b-container>
       <br>
       <h1>Créer son autocollant</h1>
-      <br>
-      <div class="card">
-        <div class="card-body">
-        <br>
-        <b-form @submit.prevent="saveStickers" class="create_stickers">
-          <b-row>
-            <b-col>
-              <label for="theme">Choisir un autocollant :</label>
-              <select id="theme" v-model="theme" title="Choix du thème" class="form-control">
-                <option value="motoFemme">Moto femme</option>
-                <option value="motoFlamme">Moto flamme</option>
-                <option value="kawasaki">Kawasaki</option>
-              </select>
-            </b-col>
+      <b-form @submit.prevent="saveStickers" class="create_stickers">
+        <div class="card">
+          <div class="card-body">
+            <b-row>
+              <b-col>
+                <label for="theme">Choisir un autocollant :</label>
+                <select id="theme" v-model="theme" title="Choix du thème" class="form-control">
+                  <option value="motoFemme">Moto femme</option>
+                  <option value="motoFlamme">Moto flamme</option>
+                  <option value="kawasaki">Kawasaki</option>
+                </select>
+              </b-col>
 
-            <b-col>
-              <label for="shape">Choisir la forme :</label>
-              <select id="shape" v-model="shape" title="Formes" class="form-control">
-                <option value="square">Carré</option>
-                <option value="round">Rond</option>
-              </select>
-            </b-col>
-          </b-row>
+              <b-col>
+                <label for="shape">Choisir la forme :</label>
+                <select id="shape" v-model="shape" title="Formes" class="form-control">
+                  <option value="square">Carré</option>
+                  <option value="round">Rond</option>
+                </select>
+              </b-col>
+            </b-row>
 
-          <b-row>
-            <b-col>
-              <label for="color">Choisir la couleur :</label>
-              <select id="color" v-model="color" title="Couleurs" class="form-control">
-                <option value="black">Noir</option>
-                <option value="red">Rouge</option>
-                <option value="yellow">Jaune</option>
-              </select>
-            </b-col>
+            <b-row>
+              <b-col>
+                <label for="color">Choisir la couleur :</label>
+                <select id="color" v-model="color" title="Couleurs" class="form-control">
+                  <option value="black">Noir</option>
+                  <option value="red">Rouge</option>
+                  <option value="yellow">Jaune</option>
+                </select>
+              </b-col>
 
-            <b-col>
-              <label for="size">Choisir la taille :</label>
-              <select id="size" v-model="size" title="Taille" class="form-control">
-                <option value="small">Petit</option>
-                <option value="medium">Moyen</option>
-                <option value="high">Grand</option>
-              </select>
-            </b-col>
-          </b-row>
+              <b-col>
+                <label for="size">Choisir la taille :</label>
+                <select id="size" v-model="size" title="Taille" class="form-control">
+                  <option value="small">Petit</option>
+                  <option value="medium">Moyen</option>
+                  <option value="high">Grand</option>
+                </select>
+              </b-col>
+            </b-row>
 
-          <b-row>
-            <b-col>
-              <h3>Aperçu</h3>
-            </b-col>
-          </b-row>
+            <b-row>
+              <b-col>
+                <h3>Aperçu</h3>
+              </b-col>
+            </b-row>
 
-          <b-row>
-            <b-col>
-              <b-img class="stickers"
-                     :src="require(`../img/stickers/${this.theme}/${this.color}.png`)"
-                     fluid
-                     alt="Stickers picture"></b-img>
-            </b-col>
-          </b-row>
-        </b-form>
+            <b-row>
+              <b-col>
+                <b-img class="stickers"
+                       :src="require(`../img/stickers/${this.theme}/${this.color}.png`)"
+                       fluid
+                       alt="Stickers picture"></b-img>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-button class="button-validate btn-warning" type="submit">Enregistrer</b-button>
+                <p class="error" v-if="errorMessage">{{errorMessage}}</p>
+              </b-col>
+            </b-row>
+          </div>
         </div>
-      </div>
-      <br>
-      <b-row>
-        <b-col>
-          <b-button class ="button-validate btn-warning" type="submit">Enregistrer</b-button>
-          <p class="error" v-if="errorMessage">{{errorMessage}}</p>
-        </b-col>
-      </b-row>
+      </b-form>
     </b-container>
-
-  <br><br>
-
-    <footer class="container-fluid text-center">
-      <div class="row">
-        <div class="col-sm-4">
-          <img src="../img/skateboard.jpg" class="img-responsive" style="width:100%" alt="Image">
-        </div>
-
-        <div class="col-sm-4">
-          <img src="../img/utah.jpg" class="img-responsive" style="width:100%" alt="Image">
-        </div>
-
-        <div class="col-sm-4">
-          <img src="../img/ski.png" class="img-responsive" style="width:100%" alt="Image">
-        </div>
-      </div>
-    </footer>
-
   </div>
 </template>
 
@@ -118,6 +96,7 @@
     },
     methods: {
       saveStickers() {
+        console.log('SAVE');
         db.collection('stickers')
           .doc(firebase.auth().currentUser.uid)
           .collection('userStickers')
@@ -133,6 +112,7 @@
             router.push(ViewMyStickers);
           })
           .catch((error) => {
+            console.log(error);
             this.errorMessage = error;
           });
       },
@@ -145,13 +125,15 @@
   .stickers {
     max-width: 500px;
   }
+
   .error {
     color: #5e0000;
     font-size: 12px;
   }
 
-  h1{
+  h1 {
     color: white;
+    margin: 1em 0;
   }
 
   .button-validate {
